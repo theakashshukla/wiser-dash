4246b30b0fea4b6c681fe300733bf352dac66e5b
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-09-28 09:48:43 +0530

    Event Update

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -11,7 +11,7 @@ const EventDetails = () => {
       id: 1,
       title: "Event 1",
       description: "Description 1",
-      date: "2021-08-01",
+      date: "2023-11-01",
       time: "10:00",
       duration: 60,
       guests: [
@@ -26,8 +26,8 @@ const EventDetails = () => {
       id: 2,
       title: "Event 2",
       description: "Description 2",
-      date: "2021-08-02",
-      time: "10:00",
+      date: "2023-08-02",
+      time: "5:00",
       duration: 60,
       guests: [
         {
@@ -46,7 +46,7 @@ const EventDetails = () => {
       id: 3,
       title: "Event 3",
       description: "Description 2",
-      date: "2021-08-02",
+      date: "2023-12-02",
       time: "10:00",
       duration: 60,
       guests: [
@@ -66,6 +66,23 @@ const EventDetails = () => {
     // Handle the case where the event is not found, e.g., display a message or redirect
     return <div>Event not found</div>;
   }
+
+  const eventDate = new Date(event.date);
+
+  // Format the date and time
+  const formattedDate = eventDate.toLocaleDateString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  const formattedTime = eventDate.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
   return (
     <div className="container mx-auto p-4 mb-9">
       <h1 className="text-blue-800 text-4xl font-bold mb-4">Ongoing Events</h1>
@@ -73,18 +90,27 @@ const EventDetails = () => {
         <div className="flex justify-between">
           <div>
             <p className="font-semibold text-gray-600 text-base">
-              Date: {event.date}
+              Date: {formattedDate}
             </p>
           </div>
           <div>
             <p className="font-semibold text-gray-600 text-base">
-              Time: {event.time}
+              Time: {formattedTime}
             </p>
           </div>
         </div>
-        <h1>{event.title}</h1>
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="font-semibold text-blue-500 text-xl">
+              {event.title}
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-gray-600 text-xl">H: 5</p>
+          </div>
+        </div>
         <p>{event.description}</p>
-        
+
         <p>Duration: {event.duration} minutes</p>
         <div>
           <h3>Guests:</h3>

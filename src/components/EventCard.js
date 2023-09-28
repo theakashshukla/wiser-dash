9877bed98b678c991814e26f5e26ed031932c431
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-09-28 10:52:05 +0530

    Update

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { FaBell } from "react-icons/fa";
 const EventCard = ({ event }) => {
   const eventDate = new Date(`${event.date} ${event.time}`);
 
@@ -19,11 +20,35 @@ const EventCard = ({ event }) => {
   return (
     <Link to={`/event/${event.id}`} className="hover:border-blue-800">
       <div className="w-196 h-374 border border-gray-300 rounded-lg p-4 mb-5">
-        <h1>{event.title}</h1>
+      <div className="flex justify-between items-center mb-1">
+          <div>
+            <h1 className="font-bold text-blue-500 text-2xl">{event.title}</h1>
+          </div>
+          <div>
+            <p className="font-semibold text-gray-800 text-sm"><p>{formattedDate}</p></p>
+          </div>
+        </div>
         <p>{event.description}</p>
-        <p>{formattedDate}</p>
+        
         <p>STARTED: {formattedTime}</p>
         <p>{event.duration}</p>
+
+        <div className="flex justify-between mt-4 mb-4">
+          <div>
+            <p className="font-semibold text-gray-800 text-xl">H: 5</p>
+          </div>
+          <button className="h-25 w-105 bg-gray-300 text-gray-600 text-sm border border-blue-800 px-2 py-1 rounded-lg transition-colors hover:bg-gray-400 hover:text-gray-800 uppercase">
+            <div className="flex items-center">
+              <FaBell className="mr-2 text-blue-800" />{" "}
+              {/* Add the bell icon and set text color to blue */}
+              <span className="text-blue-800">2</span>{" "}
+              {/* Set text color to blue */}
+            </div>
+          </button>
+          <button className="h-30 w-155 bg-blue-800 text-white text-lg border border-blue-800 px-2 py-1 rounded-lg transition-colors hover:bg-blue-600 hover:text-gray-100 uppercase">
+            Manage Event
+          </button>
+        </div>
         <div>
           <h3>Guests:</h3>
           {event.guests &&
